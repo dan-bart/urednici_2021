56774e840733d739f768f4a8a1d93f6b89fa8c60
dan-bart <dan.bartusek@seznam.cz>
2021-06-06 15:45:35 +0200

    zaprácování připomínek, nový df summary, snapshot renv

--- R/divideMain.R
+++ R/divideMain.R
@@ -1,4 +1,7 @@
+
 divide_sections <- function(df, sheet_name, section_names){
+  num_names<-c("prostredky_na_platy_a_oppp","oppp","prostredky_na_platy","prumerny_plat","schv_ke_schv","skut_k_rozp","skut_ke_skut")
+  int_names<-c("rok","kap_num","poradi_prumerneho_platu","pocet_zamestnancu")
   res = data.frame()
 
   #řádek obsahující "CELKEM" je poslední řádek, který chceme sbírat
@@ -9,7 +12,7 @@ divide_sections <- function(df, sheet_name, section_names){
     #hledáme začátek další sekce
     if(is.na(df[1,i]) || grepl("INDEX",df[1,i])) {next}
     # upravený rozpočet 2021 ještě v datech není, takže ho nepotřebujeme zpracovat
-    if(grepl("2021",df[1,i]) && grepl("UPRAV",df[1,i])) {return(res)}
+    if(grepl("2021",df[1,i]) && grepl("UPRAV",df[1,i])) {break}
     ncols = 1 #počet sloupců vymezujících danou sekci
     for (j in i+1:ncol(df)){
       if (is.na(df[1,j]) || grepl("INDEX",df[1,j]))
@@ -20,10 +23,12 @@ divide_sections <- function(df, sheet_name, section_names){
         break
       }
     }
+
     upper_index = i+ncols-1
 
     section <- df[6:nrow(df),c(1,2,i:upper_index)]
-    section[nrow(section),1]<-section[nrow(section)-1,1]
+    section[nrow(section),2]<-section[nrow(section)-1,1]
+    section[nrow(section),1]<-NA
     section<-section[-c(nrow(section)-1),]
 
 
@@ -56,5 +61,71 @@ divide_sections <- function(df, sheet_name, section_names){
     colnames(section) <- section_names
     res<- rbind(res, section)
   }
+  res<- res %>%
+    separate(Rozpocet_a_rok, into = c("typ_rozpoctu", "rok"), sep = -4, remove = TRUE) %>%
+    clean_names()
+  # res[, c(num_names,int_names)][is.na(res[,c(num_names,int_names)])] <- 0
+  res[num_names]<-sapply(res[num_names],as.numeric)
+  res[int_names]<-sapply(res[int_names],as.integer)
+  return(res)
+}
+
+
+divide_summary <- function(df, sheet_name, section_names){
+  num_names<-c("prostredky_na_platy_a_oppp","oppp","prostredky_na_platy","prumerny_plat","schv_ke_schv","skut_k_rozp","skut_ke_skut")
+  int_names<-c("rok","poradi_prumerneho_platu","pocet_zamestnancu")
+  res = data.frame()
+  for (i in 3:ncol(df)){
+    #hledáme začátek další sekce
+    if(is.na(df[1,i]) || grepl("INDEX",df[1,i])) {next}
+    # upravený rozpočet 2021 ještě v datech není, takže ho nepotřebujeme zpracovat
+    if(grepl("2021",df[1,i]) && grepl("UPRAV",df[1,i])){break}
+    ncols = 1 #počet sloupců vymezujících danou sekci
+    for (j in i+1:ncol(df)){
+      if (is.na(df[1,j]) || grepl("INDEX",df[1,j]))
+      {
+        ncols = 1+ncols
+      }
+      else{ #narazili jsme na název další sekce
+        break
+      }
+    }
+    upper_index = i+ncols-1
+    section <- df[6:nrow(df),c(2,i:upper_index)]
+
+    #SKUTEČNOST YYYY má na konci porovnání indexu platu k rozpracovanému a ke skutečnému platu minulého roku
+    if(grepl("SKUT",df[1,i]) && ncol(section) == 10){
+      section<-section[,-10]
+      section<-add_column(section, NA, .after = 7)
+    }
+
+    #SCHV má porovnání se schváleným rozpočtem minulého roku
+    else if(grepl("SCHV",df[1,i]) && ncol(section) == 8){
+      section<-add_column(section, NA, .after = 8)
+      section<-add_column(section, NA, .after = 9)
+    }
+    #UPRAVENY rozpočet nemá porovnání
+    else if(grepl("UPRAV",df[1,i]) && ncol(section) == 7){
+      section<-add_column(section, NA, .after = 7)
+      section<-add_column(section, NA, .after = 8)
+      section<-add_column(section, NA, .after = 9)
+    }
+    else{
+      print(ncol(section))
+      print(head(section))
+      print(sheet_name)
+      print(df[1,i])
+    }
+
+    section<-cbind(a = df[1,i], section)
+    colnames(section) <- section_names
+    res<- rbind(res, section)
+  }
+  res<- res %>%
+    separate(Rozpocet_a_rok, into = c("typ_rozpoctu", "rok"), sep = -4, remove = TRUE) %>%
+    clean_names()
+  #res[, c(num_names,int_names)][is.na(res[,c(num_names,int_names)])] <- 0
+  res[num_names]<-sapply(res[num_names],as.numeric)
+  res[int_names]<-sapply(res[int_names],as.integer)
   return(res)
 }
--- standardize_input_data.R
+++ standardize_input_data.R
@@ -1,11 +1,10 @@
-rm(list=ls())
-
 library(dplyr)
 library(readr)
 library(readxl)
 library(tibble)
 library(tidyr)
 library(here)
+library(janitor)
 source(here::here('R', 'listSheets.R'))
 source(here::here('R', 'divideMain.R'))
 
@@ -23,21 +22,24 @@ input<-read_excel_allsheets("./data-input/data_2021.xls")
 sapply(input,ncol)
 sapply(input,nrow)
 # OSS SS - jednotl: chybí index průměrného platu za schv.rozpočtem každého roku
-# ÚČ_S_5022 a SUMÁŘ - jiný formát
 # jednotlivci - musíme si odsouhlasit jak to zpracovat
 # zbytek přidáme
 
 
 #sheets that are in similar format
-uni_names <- c("ROPO CELKEM","OSS (RO)","PO","OOSS","STÁTNÍ SPRÁVA","ÚO","OSS SS",
-               "SOBCPO","ZAMCI_5011_platy","VOJACI_5012","ST_ZAMCI_5013","ST_ZÁSTUP_5014")
-input_uni <- vector(mode = "list")
+main_sheets <- c("ROPO CELKEM","OSS (RO)","PO","OOSS","STÁTNÍ SPRÁVA","ÚO","OSS SS",
+               "SOBCPO")
+main_names <- vector(mode = "list")
 for (name in names(input)) {
   print(name)
-  if (name %in% uni_names){
-    input_uni<-append(input_uni, input[name])
+  if (name %in% main_sheets){
+    main_names<-append(main_names, input[name])
   }
 }
+
+#TBD: jak využít data z jednotlivých skupin (popis na https://monitor.statnipokladna.cz/datovy-katalog/ciselniky/prohlizec/28)
+subgroup_sheets<-c("ZAMCI_5011_platy","VOJACI_5012","ST_ZAMCI_5013","ST_ZÁSTUP_5014","ÚČ_S_5022","SOBCPO  JEDNOTLIVÝ","OSS SS - jednotl")
+
 
 #every information that we will gather from the source excel file
 section_names <- c("Rozpocet_a_rok","Skupina","kap_num","kap_name","Prostředky na platy a OPPP", "OPPP", "Prostředky na platy",
@@ -45,13 +47,19 @@ section_names <- c("Rozpocet_a_rok","Skupina","kap_num","kap_name","Prostředky 
 main_df<-data.frame(matrix(ncol =  length(section_names), nrow = 0))
 colnames(main_df)<-section_names
 
-
-
-for (i in 1:length(input_uni)){
-  res <- divide_sections(input_uni[[i]],names(input_uni[i]),section_names)
+for (i in 1:length(main_names)){
+  res <- divide_sections(main_names[[i]],names(main_names[i]),section_names)
   main_df <- rbind(main_df, res)
 }
 
-main_df<-main_df %>% separate(Rozpocet_a_rok, into = c("Rozpocet", "Rok"), sep = -4, remove = TRUE)
-saveRDS(main_df, file = "./data-interim/main_df.rds")
+#also load and clean the summary for all sections
+summary_names <- c("Rozpocet_a_rok","Skupina","Prostředky na platy a OPPP", "OPPP", "Prostředky na platy",
+                   "Počet zaměstnanců", "Průměrný plat", "Pořadí průměrného platu","Schv ke schv", "Skut k rozp", "Skut ke skut")
+
+summary <- divide_summary(input[[length(input)]],"Summary",summary_names)
+
+
+#save both dataframes
+saveRDS(main_df, file = "./data-interim/sections.rds")
+saveRDS(summary, file = "./data-interim/summary.rds")
 
