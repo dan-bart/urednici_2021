e2848f12d04e1ae33be2833eaf1bfe724bf47981
Dan Bart <95346725@fsv.cuni.cz>
2021-07-20 20:21:49 +0200

    prvni pokus o sjednoceni dat pro roky 2004-2013 a 2013-2020

--- standardize_input_data.R
+++ standardize_input_data.R
@@ -13,6 +13,10 @@ source(here::here('R', 'divideMain.R'))
 
 #load new data
 input<-read_excel_allsheets("./data-input/data_2021.xls")
+#load old data
+chapters_old <- read.csv("~/urednici_2021/data-output/legacy/chapters_ALL.csv", encoding="UTF-8")
+grp_old <- read.csv("~/urednici_2021/data-output/legacy/groups_ALL.csv", encoding="UTF-8")
+
 sapply(input,ncol)
 sapply(input,nrow)
 
@@ -81,6 +85,82 @@ summary_names <- c("Rozpocet_a_rok","full_name","Prostředky na platy a OPPP", "
 
 summary <- divide_summary(input[[length(input)]],"Summary",summary_names)
 
+
+#now we will extend the new datasets with data from previous years
+
+#chapter dataframe
+kap_slovnik <- main_df %>% select(kap_num,kap_name,full_kap_name) %>% filter(!is.na(kap_num)) %>% unique()
+df_addition <- data.frame(c(347,338,341),c("KCP","MI","UVIS"),c("komise_pro_cenne_papiry","ministesrvto_informatiky","urad_pro_verejne_informacni_systemy"))
+names(df_addition)<-c("kap_num","kap_name","full_kap_name")
+kap_slovnik<-rbind(kap_slovnik,df_addition)
+
+
+old_dt<-chapters_old %>% select(rok = Year, name = grp, kap_num = KapNum,variable,value) %>%
+  mutate(typ_rozpoctu = case_when(str_detect(variable,"schvaleny") ~ "SCHV",
+                                  str_detect(variable,"upraveny") ~ "UPRAV",
+                                  str_detect(variable,"skutecnost") ~ "SKUT")) %>%
+  mutate(variable = case_when(str_detect(variable,"schvaleny") ~ str_remove(variable, "_schvaleny"),
+                                  str_detect(variable,"upraveny") ~ str_remove(variable, "_upraveny"),
+                                  str_detect(variable,"skutecnost") ~ str_remove(variable, "_skutecnost"))) %>%
+  filter(!is.na(typ_rozpoctu))%>% filter(!is.na(value)) %>% filter(name != "Exekutiva")%>%
+  mutate(name = case_when( name == "SOBCPO" ~ "SOBCPO",
+                          name == "St. sprava se SOBCPO" ~ "SS",
+                          name == "UO - Ministerstva" ~ "UO",
+                          name == "UO - Ostatní" ~ "UO",
+                          name == "UO" ~ "UO",
+                          name == "OSS-RO" ~ "OSS",
+                          name == "OSS-SS"~"OSS_SS",
+                          name == "PO"~"PO",
+                          name == "OOSS"~"OOSS",
+                          name =="ROPO celkem" ~ "ROPO")) %>%
+  mutate(rok = substr(rok, 1, 4)) %>%
+  reshape(timevar = "variable",idvar = c("rok","name","kap_num","typ_rozpoctu"), direction = "wide") %>%
+  rename("prostredky_na_platy_a_oppp" = value.PlatyOPPP,
+         "prostredky_na_platy" = "value.Platy",
+         "oppp" = value.OPPP,
+         "pocet_zamestnancu" =  value.Zam,
+         "prumerny_plat" = value.AvgSal) %>%
+  left_join(kap_slovnik,by = "kap_num") %>%
+  mutate (skut_k_rozp = NA, schv_ke_schv = NA, skut_ke_skut = NA,poradi_prumerneho_platu = NA) %>%
+  filter(rok != 2013)
+unique(main_df$rok)
+
+main_df<-rbind(main_df,old_dt)
+
+#group dataframe
+
+grp_slovnik <- summary %>% select(name,full_name) %>% unique()
+
+old_dt<-grp_old %>% select(rok = Year, name = grp, variable,value) %>%
+  mutate(typ_rozpoctu = case_when(str_detect(variable,"schvaleny") ~ "SCHV",
+                                  str_detect(variable,"upraveny") ~ "UPRAV",
+                                  str_detect(variable,"skutecnost") ~ "SKUT")) %>%
+  mutate(variable = case_when(str_detect(variable,"schvaleny") ~ str_remove(variable, "_schvaleny"),
+                              str_detect(variable,"upraveny") ~ str_remove(variable, "_upraveny"),
+                              str_detect(variable,"skutecnost") ~ str_remove(variable, "_skutecnost"))) %>%
+  filter(!is.na(typ_rozpoctu))%>% filter(!is.na(value)) %>% filter(name != "Exekutiva")%>%
+  mutate(name = case_when( name == "SOBCPO" ~ "SOBCPO",
+                           name == "St. sprava se SOBCPO" ~ "SS",
+                           name == "UO - Ministerstva" ~ "UO",
+                           name == "UO - Ostatní" ~ "UO",
+                           name == "UO" ~ "UO",
+                           name == "OSS-RO" ~ "OSS",
+                           name == "OSS-SS"~"OSS_SS",
+                           name == "PO"~"PO",
+                           name == "OOSS"~"OOSS",
+                           name =="ROPO celkem" ~ "ROPO")) %>%
+  mutate(rok = substr(rok, 1, 4)) %>%
+  reshape(timevar = "variable",idvar = c("rok","name","typ_rozpoctu"), direction = "wide") %>%
+  rename("prostredky_na_platy_a_oppp" = value.PlatyOPPP,
+         "prostredky_na_platy" = "value.Platy",
+         "oppp" = value.OPPP,
+         "pocet_zamestnancu" =  value.Zam,
+         "prumerny_plat" = value.AvgSal) %>%
+  left_join(grp_slovnik,by = "name") %>%
+  mutate (skut_k_rozp = NA, schv_ke_schv = NA, skut_ke_skut = NA,poradi_prumerneho_platu = NA) %>%
+  filter(rok != 2013)
+
+summary<-rbind(summary,old_dt)
 
 #save all dataframes
 saveRDS(main_df, file = "./data-interim/sections.rds")
