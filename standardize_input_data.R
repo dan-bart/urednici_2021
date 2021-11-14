32a83d018991756ed765ed85b39be55fba27754d
danbart <dan.bartusek@seznam.cz>
2021-11-14 15:55:01 +0100

    update

--- standardize_input_data.R
+++ standardize_input_data.R
@@ -247,8 +247,18 @@ df_infl <- data.frame(
   rok = seq(2003, 2020),
   inflation = c(0.1, 2.8, 1.9, 2.5, 2.8, 6.3, 1.0, 1.5, 1.9, 3.3, 1.4, 0.4, 0.3, 0.7, 2.5, 2.1, 2.8, 3.2) / 100 + 1
 )
-df_infl$base_2003 <- NA
-df_infl$base_2020 <- NA
+
+df_infl <- czso_get_table("010022", dest_dir = "data-input/czso")%>% filter(is.na(ucel_txt)) %>%
+  filter(casz_txt == "stejné období předchozího roku") %>%
+  group_by(rok) %>%
+  summarise(hodnota = mean(hodnota)) %>%
+  select(contains("obdobi"),rok, hodnota) %>%
+  mutate(inflation=hodnota/100)%>%
+  arrange(rok)%>%
+  filter(rok>=2003,rok<=2020)
+
+df_infl$base_2003 <- 0
+df_infl$base_2020 <- 0
 df_infl[1, "base_2003"] <- 1
 df_infl[nrow(df_infl), "base_2020"] <- 1
 
