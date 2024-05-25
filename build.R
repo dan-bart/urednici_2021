008167c813a32c45218767394e59b72386d0c7b9
Petr Bouchal <pbouchal@gmail.com>
2024-05-25 16:25:44 +0200

    update build file

--- build.R
+++ build.R
@@ -1,8 +1,12 @@
 source("standardize_input_data.R")
+
+source("R/export_data.R")
+rmarkdown::render("codebook.Rmd")
+
+quarto::quarto_render("szu-analyza-stsl.qmd")
+
 rmarkdown::render("results.Rmd")
 source("graphs.R")
 source("graphs_mod.R")
-source("export_data.R")
-rmarkdown::render("codebook.Rmd")
 
 system2("http-server", args = c("graphs_mod" , "--o"))
