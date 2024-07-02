017181d4626d9d631a29d70852a7cd2f750baa16
Petr Bouchal <pbouchal@gmail.com>
2024-07-02 08:19:14 +0200

    amend build script

--- build.R
+++ build.R
@@ -4,9 +4,11 @@ source("R/export_data.R")
 rmarkdown::render("codebook.Rmd")
 
 quarto::quarto_render("szu-analyza-stsl.qmd")
+system2("http-server", args = c("web_partial" , "--o"))
 
 rmarkdown::render("results.Rmd")
 source("graphs.R")
 source("graphs_mod.R")
+source("graphs_mod_long.r")
 
 system2("http-server", args = c("graphs_mod" , "--o"))
