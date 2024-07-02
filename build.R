498b62b9627770fbd35e9e46709f5cb84fab700c
Petr Bouchal <pbouchal@gmail.com>
2024-07-02 12:00:18 +0200

    more handy build commands

--- build.R
+++ build.R
@@ -5,6 +5,13 @@ rmarkdown::render("codebook.Rmd")
 
 quarto::quarto_render("szu-analyza-stsl.qmd")
 system2("http-server", args = c("web_partial" , "--o"))
+system2("netlify", args = c("deploy",
+                            "--dir", "web_partial" ,
+                            "--site", "curious-profiterole-ba0eee"))
+system2("netlify", args = c("deploy",
+                            "--dir", "web_partial" ,
+                            "--site", "curious-profiterole-ba0eee",
+                            "--prod"))
 
 rmarkdown::render("results.Rmd")
 source("graphs.R")
@@ -12,3 +19,10 @@ source("graphs_mod.R")
 source("graphs_mod_long.r")
 
 system2("http-server", args = c("graphs_mod" , "--o"))
+system2("netlify", args = c("deploy",
+                            "--dir", "graphs_mod" ,
+                            "--site", "studie-urednici"))
+system2("netlify", args = c("deploy",
+                            "--dir", "graphs_mod" ,
+                            "--site", "studie-urednici",
+                            "--prod"))
